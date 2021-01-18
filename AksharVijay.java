5ea6e00c0edf66329679f5e6c9f235f1eb9e07f8
Akshar Vijay <aksharvijay8@gmail.com>
2021-01-18 22:11:45 +0530

    My First Java program on GIT!

---
+++ AksharVijay.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Akshar
+{
+    public static void main(string[]args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int a,b;
+
+        for(a=1;a<=5;a++)
+        {
+            for(b=1;b<=a;b++)
+            {
+                System.out.println("*");
+            }
+        }
+    }
+}
