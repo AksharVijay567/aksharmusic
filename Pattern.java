0ad277da86c1694c78c63fca5888258959db433a
Akshar Vijay <aksharvijay8@gmail.com>
2021-01-18 22:19:08 +0530

    Akshar Vijay

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+import java.util.Scanner\
+public class Pattern
+{
+    public static void main (String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+
+        for(a=1;a<=10;a++)
+        {
+            System.out.println(a);
+        }
+    }
+}
